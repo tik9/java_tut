292c43f3bbeaaac527aa077cec3955c64ec85e9f
ti ko <thome@tis-Mac>
2025-09-18 23:07:35 +0200

    Mails.java-working-and-concise

--- tk/Mails.java
+++ tk/Mails.java
@@ -3,29 +3,39 @@ package tk;
 import java.util.Random;
 
 public class Mails {
-    static final String[] WOCHENTAGE = { "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag",
-            "Sonntag" };
+    static final String[] WOCHENTAGE = { "Mo", "Di", "Mi", "Do", "Fr", "Sa", "So" };
     static final int[] MAILS_PRO_TAG = { 12, 15, 22, 13, 0, 21, 19 };
-    static final int ziel = 500;
+    static final int ZIEL = 500;
+    static final Random random = new Random();
 
     public static void main(String[] args) {
-        int mails = 0;
+        int gesamtKunden = 0;
         int woche = 0;
-        String tag = "";
+        String erreichtTag = "";
+        int erreichtWoche = 0;
+        boolean zielErreicht = false;
 
-        while (tag=="") {
+        while (!zielErreicht) {
+            woche++;
+            int kundenDieseWoche = 0;
+
             for (int i = 0; i < 7; i++) {
-                mails += MAILS_PRO_TAG[i] - (new Random()).nextInt(3, 11);
-                if (mails >= ziel) {
-                    tag = WOCHENTAGE[i];
-                    break;
+                int nichtReagiert = random.nextInt(8) + 3; // 3-10
+                int kundenHeute = MAILS_PRO_TAG[i] - nichtReagiert;
+                kundenDieseWoche += Math.max(kundenHeute, 0); // Negative vermeiden
+
+                if (!zielErreicht && gesamtKunden + kundenDieseWoche >= ZIEL) {
+                    zielErreicht = true;
+                    erreichtTag = WOCHENTAGE[i];
+                    erreichtWoche = woche;
                 }
             }
-            woche++;
 
-            System.out.println(mails + " Mails nach Woche " + woche);
+            gesamtKunden += kundenDieseWoche;
+            System.out.println("Nach der " + woche + ". Woche hat Fred " + gesamtKunden + " Neukunden");
         }
-        System.out.println("Ziel von " + ziel + " Neukunden erreicht am " + tag + " in " + woche + ". Woche (gesamt "
-                + mails + " Kunden)");
+
+        System.out.println("Ziel von " + ZIEL + " Neukunden erreicht/Überschritten am " + erreichtTag +
+                " der " + erreichtWoche + ". Woche (gesamt: " + gesamtKunden + " Neukunden).");
     }
 }

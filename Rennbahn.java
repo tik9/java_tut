e112fd9a69f6e8ac5ee0f5d005a1c05ab18a0260
ti ko <thome@tis-Mac>
2025-11-23 22:40:53 +0100

    rennbahn-hinzu

--- Rennbahn.java
+++ Rennbahn.java
@@ -14,18 +14,9 @@ public class Rennbahn {
             spuren[anzahlAutos] = auto;
             anzahlAutos++;
             System.out.println(auto.fahrerName + " wurde auf Spur " + anzahlAutos + " gesetzt");
-            System.out.println("Keine freien Spuren mehr für " + auto.fahrerName);
     }
     
-    public void simuliereZeitabschnitt() {
-        System.out.println("Neuer Zeitabschnitt");
-        for (int i = 0; i < anzahlAutos; i++) {
-            if (spuren[i] != null) {
-                spuren[i].fahre();
-            }
-        }
-    }
-    
+
     public Rennauto liefereSieger() {
         Rennauto sieger = null;
         double maxStrecke = 0;
@@ -33,8 +24,7 @@ public class Rennbahn {
         for (int i = 0; i < anzahlAutos; i++) {
             if (spuren[i] != null && spuren[i].gefahreneStrecke >= streckenlaenge) {
                 if (spuren[i].gefahreneStrecke > maxStrecke) {
-                    maxStrecke = spuren[i].gefahreneStrecke;
-                    sieger = spuren[i];
+//tbd
                 }
             }
         }
@@ -45,14 +35,7 @@ public class Rennbahn {
         System.out.println("Start");
         System.out.println("Streckenlänge: " + streckenlaenge);
         
-        int runde = 1;
-        while (liefereSieger() == null) {
-            System.out.println("\nRunde " + runde );
-            simuliereZeitabschnitt();
-            runde++;
-        }
-        
-        Rennauto sieger = liefereSieger();
+     //tbd   
         System.out.println("\nWinner: " + sieger.fahrerName);
         System.out.println("Endstrecke: " + sieger.gefahreneStrecke);
     }
